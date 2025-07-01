dd74b1cd90078c9e3d39c3360ab566e0e41301a9
Meas Sophanith <sophanithmeas91@gmail.com>
2025-07-01 13:35:02 +0700

    Add assets names to fix the merge conflict

--- main.ts
+++ main.ts
@@ -1,242 +1,50 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue`)
         game.showLongText("Hello dear! Let me introduce you what you need to know before starting", DialogLayout.Bottom)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue2`)
         game.showLongText("First, you are a newly born AI which you need to learn as you grow up.", DialogLayout.Bottom)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile13`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue8`)
         game.showLongText("You can play 1 VS 1", DialogLayout.Bottom)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue5`)
         game.showLongText("After finish all the learning phase you will become a powerful AI who can do a lot of tasks", DialogLayout.Bottom)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue4`)
         game.showLongText("The more you learn, the smarter you are", DialogLayout.Bottom)
         game.showLongText("The more you learn, the smarter you are", DialogLayout.Bottom)
     }
 })
 function box2 (location: Image, floor: Image) {
     for (let value of tiles.getTilesByType(location)) {
-        box = sprites.create(img`
-            . . b b b b b b b b b b b b . . 
-            . b e 4 4 4 4 4 4 4 4 4 4 e b . 
-            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-            b e e 4 4 4 4 4 4 4 4 4 4 e e b 
-            b e e e e e e e e e e e e e e b 
-            b e e e e e e e e e e e e e e b 
-            b b b b b b b d d b b b b b b b 
-            c b b b b b b c c b b b b b b c 
-            c c c c c c b c c b c c c c c c 
-            b e e e e e c b b c e e e e e b 
-            b e e e e e e e e e e e e e e b 
-            b c e e e e e e e e e e e e c b 
-            b b b b b b b b b b b b b b b b 
-            . b b . . . . . . . . . . b b . 
-            `, SpriteKind.Player)
+        box = sprites.create(assets.image`Chest`, SpriteKind.Player)
         tiles.placeOnTile(box, value)
         tiles.setTileAt(value, floor)
     }
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue6`)
         game.showLongText("After learning, guess what you can do next?", DialogLayout.Bottom)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue10`)
         game.showLongText("That's it for what you need to know. Now enjoy your journey", DialogLayout.Bottom)
     }
 })
@@ -248,94 +56,19 @@ function spawnpoint () {
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue3`)
         game.showLongText("You will explore and consume the knowledge.", DialogLayout.Bottom)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue7`)
         game.showLongText("You can compete with other friends too", DialogLayout.Bottom)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile14`, function (sprite, location) {
     if (controller.B.isPressed()) {
-        game.setDialogFrame(img`
-            .....cccccccccccccc.....
-            ...cbd111111111111dbc...
-            ..cd1111111111111111dc..
-            .cd111111111111111111dc.
-            .b11111111111111111111b.
-            cd11111111111111111111dc
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            c1111111111111111111111c
-            cd11111111111111111111dc
-            cb11111111111111111111bc
-            ccd111111111111111111dc.
-            .ccd1111111111111111dcc.
-            ..c111111111111111dbcc..
-            .b11dcccccccccccccccc...
-            cddcccccccccccccccc.....
-            ccccc...................
-            `)
+        game.setDialogFrame(assets.image`Dialogue9`)
         game.showLongText("Suprisingly you can also play 2 VS 2", DialogLayout.Bottom)
     }
 })
